64bf9e7d32c5d5f3e72342aed957db7b4775298a
YezhenWang <309426989@qq.com>
2019-03-02 23:28:13 +0800

    23:28 03/02/2019 in harbin

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class DefaultConfig(object):
     train_data_path = "/home/zzn/part_B_final/train_data/images_train.npy"
-    test_data_path = ""
+    test_data_path = "/home/zzn/part_B_final/test_data/images_test.npy"
     validate_data_path = "/home/zzn/part_B_final/train_data/images_validate.npy"
 
     train_gt_path = "/home/zzn/part_B_final/train_data/gt_train.npy"
-    test_gt_path = ""
+    test_gt_path = "/home/zzn/part_B_final/test_data/gt_test.npy"
     validate_gt_path = "/home/zzn/part_B_final/train_data/gt_validate.npy"
 
     batch_size = 1
@@ -15,3 +15,5 @@ class DefaultConfig(object):
     lr = 1e-5
     lr_decay = 0.95
     weight_decay = 1e-4
+
+    model_save_path = "/home/zzn/ADCrowd_pytorch/checkpoints/model.pkl"
